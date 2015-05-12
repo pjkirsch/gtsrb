4bb8bdc2174b7a75dc126460c7ae94c6d67f986d
PJ Kirsch <pjkirsch@live.com>
2015-05-12 17:21:36 +0200

    Change comments

--- scripts/models/mlp.lua
+++ scripts/models/mlp.lua
@@ -1,4 +1,4 @@
--- Define a neural network model with 2 hidden layer and 1 softmax layer
+-- Define a toy neural network model with 1 hidden layer and 1 softmax layer
 
 -- Load libraries
 require 'torch'
@@ -11,17 +11,14 @@ depth = 3
 
 nInputs = height*width*depth
 nHL1 = 30
-nHL2 = 30
 nOutputs = 43
 
 -- Define model
 model = nn.Sequential() -- The NN model is considered as a sequence of functions
-model:add(nn.Reshape(nInputs)) -- Reshape features from 28*28 to 784*1
+model:add(nn.Reshape(nInputs)) -- Reshape features from 3*32*32 to 3072*1
 model:add(nn.Linear(nInputs, nHL1)) -- Connection weights
 model:add(nn.Tanh()) -- Hyperbolic tangent non-linear function
---model:add(nn.Linear(nHL1, nHL2)) -- Connection weights
---model:add(nn.Tanh()) -- Hyperbolic tangent non-linear function
-model:add(nn.Linear(nHL2, nOutputs)) -- Connection weights
+model:add(nn.Linear(nHL1, nOutputs)) -- Connection weights
 model:add(nn.LogSoftMax())
 
 -- Display model description
