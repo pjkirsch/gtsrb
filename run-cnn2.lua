f341721f2dab8dfe65be794ab934b85d73e2bf50
PJ Kirsch <pjkirsch@live.com>
2015-05-28 14:54:58 +0200

    Change main running scripts to automatically generate datasets preprocessing

--- run-cnn2.lua
+++ run-cnn2.lua
@@ -2,20 +2,31 @@ require "scripts/funcs"
 
 require "torch"
 
---dofile("scripts/loadTrain.lua")
---dofile("scripts/loadTest.lua")
-trainData = torch.load('data/trainDataGrey.t7')
-validData = torch.load('data/validDataGrey.t7')
-testData = torch.load('data/testDataGrey.t7')
+-- Generate the datasets if not generated yet
+if fileExists("data/trainDataGrey.t7") and fileExists("data/validDataGrey.t7") then
+    trainData = torch.load('data/trainDataGrey.t7')
+    validData = torch.load('data/validDataGrey.t7')
+else
+    dofile("scripts/loadTrainGrey.lua")
+end
 
+if fileExists("data/testDataGrey.t7") then
+    testData = torch.load('data/testDataGrey.t7')
+else
+    dofile("scripts/loadTestGrey.lua")
+end
+
+-- Load the model
 dofile("scripts/models/cnn2.lua")
 
+-- Define the training and testing procedure
 dofile("scripts/train.lua")
 dofile("scripts/valid.lua")
 dofile("scripts/test.lua")
 
+-- Start learning procedure
 while true do
-	train()
-	valid()
-	test()
+    train()
+    valid()
+    test()
 end
